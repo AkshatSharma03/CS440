9904707084f6d25d450ef647f643d3fa7af227d4
Akshat sharma <akshat1.sunil@gmail.com>
2026-03-02 02:36:04 -0500

    completed pacman

--- src/pas/pacman/agents/PacmanAgent.java
+++ src/pas/pacman/agents/PacmanAgent.java
@@ -1,8 +1,6 @@
 package src.pas.pacman.agents;
 
-
 // SYSTEM IMPORTS
-import edu.bu.pas.pacman.agents.Agent;
 import edu.bu.pas.pacman.agents.SearchAgent;
 import edu.bu.pas.pacman.game.Action;
 import edu.bu.pas.pacman.game.Game.GameView;
@@ -11,32 +9,25 @@ import edu.bu.pas.pacman.graph.PelletGraph.PelletVertex;
 import edu.bu.pas.pacman.routing.BoardRouter;
 import edu.bu.pas.pacman.routing.PelletRouter;
 import edu.bu.pas.pacman.utils.Coordinate;
-import edu.bu.pas.pacman.utils.Pair;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
-import java.util.Set;
-
+import java.util.Stack;
 
 // JAVA PROJECT IMPORTS
-import src.pas.pacman.routing.ThriftyBoardRouter;  // responsible for how to get somewhere
-import src.pas.pacman.routing.ThriftyPelletRouter; // responsible for pellet order
+import src.pas.pacman.routing.ThriftyBoardRouter;
+import src.pas.pacman.routing.ThriftyPelletRouter;
 
-
-public class PacmanAgent
-    extends SearchAgent
-{
+public class PacmanAgent extends SearchAgent {
 
     private final Random random;
-    private BoardRouter  boardRouter;
+    private BoardRouter boardRouter;
     private PelletRouter pelletRouter;
 
-    public PacmanAgent(int myUnitId,
-                       int pacmanId,
-                       int ghostChaseRadius)
-    {
+    public PacmanAgent(int myUnitId, int pacmanId, int ghostChaseRadius) {
         super(myUnitId, pacmanId, ghostChaseRadius);
         this.random = new Random();
-
         this.boardRouter = new ThriftyBoardRouter(myUnitId, pacmanId, ghostChaseRadius);
         this.pelletRouter = new ThriftyPelletRouter(myUnitId, pacmanId, ghostChaseRadius);
     }
@@ -46,27 +37,113 @@ public class PacmanAgent
     public final PelletRouter getPelletRouter() { return this.pelletRouter; }
 
     @Override
-    public void makePlan(final GameView game)
-    {
-        // TODO: implement me! This method is responsible for calculating
-        // the "plan" of Coordinates you should visit in order to get from a starting
-        // location and another ending location. I recommend you use
-        // this.getBoardRouter().graphSearch(...) to get a path and convert it into
-        // a Stack of Coordinates (see the documentation for SearchAgent)
-        // which your makeMove can do something with!
+    public void makePlan(final GameView game) {
+        Coordinate src = game.getEntity(game.getPacmanId()).getCurrentCoordinate();
+        Coordinate target = this.getTargetCoordinate();
+
+        // CASE 1: Specific target set (Unit Tests, specific movement)
+        if (target != null) {
+            Path<Coordinate> path = this.getBoardRouter().graphSearch(src, target, game);
+            if (path != null) {
+                Stack<Coordinate> plan = pathToStack(path);
+                this.setPlanToGetToTarget(plan);
+            } else {
+                this.setPlanToGetToTarget(new Stack<>());
+            }
+            return;
+        }
+
+        // CASE 2: No target set (Play Game: Eat all pellets efficiently)
+        // Use PelletRouter to find the best order to eat pellets
+        Path<PelletVertex> tour = this.getPelletRouter().graphSearch(game);
+        
+        if (tour != null) {
+            // Unroll the high-level tour (Goal -> Start)
+            List<PelletVertex> tourStates = new ArrayList<>();
+            Path<PelletVertex> p = tour;
+            while (p != null) {
+                tourStates.add(p.getDestination());
+                p = p.getParentPath();
+            }
+            Collections.reverse(tourStates); // Now Start -> Goal
+
+            Stack<Coordinate> fullPlan = new Stack<>();
+            
+            // Build movement plan backwards (Last pellet -> ... -> First pellet)
+            for (int i = tourStates.size() - 1; i > 0; i--) {
+                Coordinate from = tourStates.get(i - 1).getPacmanCoordinate();
+                Coordinate to = tourStates.get(i).getPacmanCoordinate();
+                
+                // Route between pellets
+                Path<Coordinate> leg = this.getBoardRouter().graphSearch(from, to, game);
+                if (leg != null) {
+                    List<Coordinate> legCoords = new ArrayList<>();
+                    Path<Coordinate> legP = leg;
+                    // Add coords, excluding the 'from' node to avoid duplicates
+                    while (legP != null && legP.getParentPath() != null) {
+                        legCoords.add(legP.getDestination());
+                        legP = legP.getParentPath();
+                    }
+                    // LegCoords is (Dest -> ... -> NextStep). 
+                    // Stack needs NextStep at top.
+                    for (Coordinate c : legCoords) {
+                        fullPlan.push(c);
+                    }
+                }
+            }
+            this.setPlanToGetToTarget(fullPlan);
+        }
+    }
+
+    // Helper to convert Path<Coordinate> to Stack<Coordinate>
+    private Stack<Coordinate> pathToStack(Path<Coordinate> path) {
+        List<Coordinate> coords = new ArrayList<>();
+        Path<Coordinate> p = path;
+        while (p != null && p.getParentPath() != null) { // Exclude source
+            coords.add(p.getDestination());
+            p = p.getParentPath();
+        }
+        // Coords: Dest -> ... -> FirstStep
+        // Stack should pop FirstStep first.
+        Stack<Coordinate> plan = new Stack<>();
+        for (Coordinate c : coords) {
+            plan.push(c);
+        }
+        return plan;
     }
 
     @Override
-    public Action makeMove(final GameView game)
-    {
-        // This is currently configured to choose a random action
-        // TODO: change me!
-        return Action.values()[this.getRandom().nextInt(Action.values().length)];
+    public Action makeMove(final GameView game) {
+        Stack<Coordinate> plan = this.getPlanToGetToTarget();
+
+        if (plan == null || plan.isEmpty()) {
+            this.makePlan(game);
+            plan = this.getPlanToGetToTarget();
+        }
+
+        if (plan == null || plan.isEmpty()) {
+            return Action.UP;
+        }
+
+        Coordinate current = game.getEntity(game.getPacmanId()).getCurrentCoordinate();
+        Coordinate next = plan.peek(); // Peek first
+
+        // Check if we are already there (edge case)
+        if (current.equals(next)) {
+            plan.pop();
+            if (plan.isEmpty()) return Action.UP;
+            next = plan.peek();
+        }
+
+        next = plan.pop();
+
+        try {
+            return Action.inferFromCoordinates(current, next);
+        } catch (Exception e) {
+            return Action.UP;
+        }
     }
 
     @Override
-    public void afterGameEnds(final GameView game)
-    {
-        // if you want to log stuff after a game ends implement me!
-    }
+    public void afterGameEnds(final GameView game) {}
 }
--- src/pas/pacman/routing/ThriftyBoardRouter.java
+++ src/pas/pacman/routing/ThriftyBoardRouter.java
@@ -1,66 +1,75 @@
 package src.pas.pacman.routing;
 
-
 // SYSTEM IMPORTS
 import java.util.Collection;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.HashSet;
 
 // JAVA PROJECT IMPORTS
-import edu.bu.pas.pacman.agents.Agent;
 import edu.bu.pas.pacman.game.Action;
 import edu.bu.pas.pacman.game.Game.GameView;
-import edu.bu.pas.pacman.game.Tile;
 import edu.bu.pas.pacman.graph.Path;
 import edu.bu.pas.pacman.routing.BoardRouter;
-import edu.bu.pas.pacman.routing.BoardRouter.ExtraParams;
 import edu.bu.pas.pacman.utils.Coordinate;
-import edu.bu.pas.pacman.utils.Pair;
 
+public class ThriftyBoardRouter extends BoardRouter {
 
-// This class is responsible for calculating routes between two Coordinates on the Map.
-// Use this in your PacmanAgent to calculate routes that (if followed) will lead
-// Pacman from some Coordinate to some other Coordinate on the map.
-public class ThriftyBoardRouter
-    extends BoardRouter
-{
-
-    // If you want to encode other information you think is useful for Coordinate routing
-    // besides Coordinates and data available in GameView you can do so here.
-    public static class BoardExtraParams
-        extends ExtraParams
-    {
-
+    public static class BoardExtraParams extends ExtraParams {
+        // No extra parameters needed for basic BFS
     }
 
-    // feel free to add other fields here!
-
-    public ThriftyBoardRouter(int myUnitId,
-                              int pacmanId,
-                              int ghostChaseRadius)
-    {
+    public ThriftyBoardRouter(int myUnitId, int pacmanId, int ghostChaseRadius) {
         super(myUnitId, pacmanId, ghostChaseRadius);
-
-        // if you add fields don't forget to initialize them here!
-    }
-
-
-    @Override
-    public Collection<Coordinate> getOutgoingNeighbors(final Coordinate src,
-                                                       final GameView game,
-                                                       final ExtraParams params)
-    {
-        // TODO: implement me!
-        return null;
     }
 
     @Override
-    public Path<Coordinate> graphSearch(final Coordinate src,
-                                        final Coordinate tgt,
-                                        final GameView game)
-    {
-        // TODO: implement me!
-        return null;
+    public Collection<Coordinate> getOutgoingNeighbors(
+        final Coordinate src, final GameView game, final ExtraParams params) {
+        List<Coordinate> neighbors = new ArrayList<>();
+        for (Action action : Action.values()) {
+            if (game.isLegalPacmanMove(src, action)) {
+                Coordinate next = src.getNeighbor(action);
+                if (!next.equals(src)) {
+                    neighbors.add(next);
+                }
+            }
+        }
+        return neighbors;
     }
 
+    @Override
+    public Path<Coordinate> graphSearch(
+        final Coordinate src, final Coordinate tgt, final GameView game) {
+        
+        if (src.equals(tgt)) return new Path<>(src);
+
+        Queue<Path<Coordinate>> frontier = new LinkedList<>();
+        Set<Coordinate> visited = new HashSet<>();
+
+        frontier.add(new Path<>(src));
+        visited.add(src);
+
+        while (!frontier.isEmpty()) {
+            Path<Coordinate> currentPath = frontier.poll();
+            Coordinate current = currentPath.getDestination();
+
+            for (Coordinate neighbor : getOutgoingNeighbors(current, game, null)) {
+                if (visited.contains(neighbor)) continue;
+
+                visited.add(neighbor);
+                // BFS cost is uniformly 1.0 per move
+                Path<Coordinate> newPath = new Path<>(neighbor, 1.0f, currentPath);
+
+                if (neighbor.equals(tgt)) return newPath;
+                
+                frontier.add(newPath);
+            }
+        }
+        return null;
+    }
 }
 
